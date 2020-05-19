f17938966eefd12fd23225eb25bfd12faaf162ad
daniellpacheco <daniel.pacheco123@hotmail.com>
2020-05-19 16:21:23 -0300

    create login screen.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # social-network
+
+Projeto de uma Rede Social feito com o intuito de estudar e treinar as seguintes tecnologias: <b>React,</b> <b>NodeJS</b> e <b>Socket.</b>
--- src/components/login.tsx
+++ src/components/login.tsx
@@ -4,23 +4,31 @@ class Login extends React.Component {
 
     render() {
         return(
-            <section>
-                <div>
-                    <img src="" alt=""/>
-                </div>
-                <div>
-                    <form action="" method="POST">
-                        <img src="" alt="Logo Cloud"/>
-                        <h1>Entrar no <span>Lumus</span></h1>
-                        <label htmlFor="email">
-                            <input type="email" name="email" id=""/>
-                        </label>
-                        <label htmlFor="senha">
-                            <input type="password" name="senha" placeholder="6+ Digitos"/>
-                        </label>
-                        <a href="">Esqueci a senha!</a>
-                        <button type="submit">Entrar no Lumus</button>
-                    </form>
+            <section className="background">
+                {/* <img src="img/back-login.png" alt=""/> */}
+                <div className="login-wrap">
+                    <div className="login-inner">
+                        <img src="img/img-login.svg" alt=""/>
+                    </div>
+                    <div className="login-inner">
+                        <form action="" method="POST" className="form-wrap">
+                            <a href="/">
+                                <img className="logo" src="img/logo.svg" alt="Logo Cloud"/>
+                            </a>
+                            <h1>Entrar na <span className="title">Cloud</span></h1>
+                            <fieldset className="input-field">
+                                <label htmlFor="email">Endereço de E-mail</label>
+                                <input type="email" name="email" id=""/>
+                            </fieldset>
+                            <fieldset className="input-field">
+                                <label htmlFor="senha">Senha</label>
+                                <input type="password" name="senha" placeholder="6+ Digitos"/>
+                            </fieldset>
+                            <a href="/">Esqueci a senha!</a>
+                            <a href="/">Ainda não possuo conta!</a>
+                            <button type="submit">Entrar no Lumus</button>
+                        </form>
+                    </div>
                 </div>
             </section>
         );
